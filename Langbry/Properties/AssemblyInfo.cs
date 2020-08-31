2689045bf357f45d76a4793a445ef2a4f1447239
miriamme <miriamme@gmail.com>
2020-08-31 10:35:31 +0900

    버전정보 업데이트

--- Langbry/Properties/AssemblyInfo.cs
+++ Langbry/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Langbry")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Language Library")]
+[assembly: AssemblyDescription("Language Library for ASP.NET Form")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Langbry")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호를
 // 기본값으로 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
